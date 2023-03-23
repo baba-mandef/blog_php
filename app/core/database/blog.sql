f80c2a602763e350f64269d1a4e48e510fa26506
ptahemdjehuty <pariso03henri@gmail.com>
2023-03-23 16:42:43 +0100

    - update database

--- app/core/database/blog.sql
+++ app/core/database/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 21, 2023 at 03:35 PM
+-- Generation Time: Mar 23, 2023 at 03:39 PM
 -- Server version: 10.10.3-MariaDB
 -- PHP Version: 8.2.2
 
@@ -34,8 +34,15 @@ CREATE TABLE `comment` (
   `post` int(11) NOT NULL,
   `is_reply` tinyint(1) NOT NULL DEFAULT 0,
   `comment` int(11) DEFAULT NULL,
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `body`, `author`, `post`, `is_reply`, `comment`, `created_at`) VALUES
+(1, 'adf', 1, 1, 0, NULL, '2023-03-22 16:51:23');
 
 -- --------------------------------------------------------
 
@@ -72,7 +79,9 @@ CREATE TABLE `post` (
 INSERT INTO `post` (`id`, `title`, `slug`, `body`, `banner`, `author`, `created_at`) VALUES
 (1, 'bienvenue sur le blog de l\'abassadeur', 'bienvenue-sur-le-blog-de-l-abassadeur', 'bienvenue sur le blog de l\'abassadeur\r\nbienvenue sur le blog de l\'abassadeur\r\nbienvenue sur le blog de l\'abassadeurbienvenue sur le blog de l\'abassadeurbienvenue sur le blog de l\'abassadeurbienvenue sur le blog de l\'abassadeurbienvenue sur le blog de l\'abassadeurbienvenue sur le blog de l\'abassadeurbienvenue sur le blog de l\'abassadeurbienvenue sur le blog de l\'abassadeur', 'app/media/0de1b2c5d4daba545722584b1d165bdf.jpg', 1, '2023-03-21 13:53:11'),
 (2, 'bienvenue sur le blog de l\'abassadeur', 'bienvenue-sur-le-blog-de-l-abassadeur', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui placeat deleniti distinctio aut quisquam ab repudiandae dolorem exercitationem quo ipsa expedita, necessitatibus assumenda a, perspiciatis ullam minus? Nisi, repellendus.\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Neque, exercitationem. Tempora accusamus, praesentium excepturi ex nemo vel mollitia magnam inventore voluptatem facilis obcaecati delectus nihil dolor quia aliquam commodi laborum!', 'app/media/0de1b2c5d4daba545722584b1d165bdf.jpg', 1, '2023-03-21 13:56:01'),
-(3, 'hdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqn', 'hdeqdioqd-qeodjqodjqo-oiqdlqwjdq-sqoqdqn', 'hdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqn', 'app/media/0b9ca181ad4bc1086509ff18a0772815.png', 1, '2023-03-21 14:17:32');
+(3, 'hdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqn', 'hdeqdioqd-qeodjqodjqo-oiqdlqwjdq-sqoqdqn', 'hdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqnhdeqdioqd qeodjqodjqo oiqdlqwjdq sqoqdqn', 'app/media/0b9ca181ad4bc1086509ff18a0772815.png', 1, '2023-03-21 14:17:32'),
+(4, 'afedfeait', 'afedfeait', 'fedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeaitfedfeait', 'app/media/0de1b2c5d4daba545722584b1d165bdf.jpg', 1, '2023-03-22 12:07:25'),
+(5, 'ola', 'ola', 'ffthcujyt', 'app/media/0e3da33f8aa36b4461e238906ea5a6bb.jpg', 1, '2023-03-22 12:08:35');
 
 -- --------------------------------------------------------
 
@@ -102,13 +111,18 @@ INSERT INTO `user` (`id`, `username`, `password`, `join_at`) VALUES
 -- Indexes for table `comment`
 --
 ALTER TABLE `comment`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_comment` (`post`),
+  ADD KEY `comment_author` (`author`),
+  ADD KEY `comment_reply` (`comment`);
 
 --
 -- Indexes for table `like_post`
 --
 ALTER TABLE `like_post`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_like` (`post`),
+  ADD KEY `like_author` (`author`);
 
 --
 -- Indexes for table `post`
@@ -132,7 +146,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `like_post`
@@ -144,7 +158,7 @@ ALTER TABLE `like_post`
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -155,6 +169,21 @@ ALTER TABLE `user`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_author` FOREIGN KEY (`author`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_reply` FOREIGN KEY (`comment`) REFERENCES `comment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_comment` FOREIGN KEY (`post`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `like_post`
+--
+ALTER TABLE `like_post`
+  ADD CONSTRAINT `like_author` FOREIGN KEY (`author`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_like` FOREIGN KEY (`post`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `post`
