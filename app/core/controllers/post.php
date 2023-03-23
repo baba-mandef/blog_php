a262710561ab6c7822116be36aa4cedc85a0e69b
ptahemdjehuty <pariso03henri@gmail.com>
2023-03-23 11:26:55 +0100

    - add details method && add_comment method

--- app/core/controllers/post.php
+++ app/core/controllers/post.php
@@ -1,58 +1,90 @@
 <?php
-    class Post{
+class Post
+{
 
-        public function add_post ($title, $body){
+    public function add_post($title, $body)
+    {
 
-                if(
-                    isset($title) && !empty($title)
-                    &&
-                    isset($body) && !empty($body)
-                ){
+        if (
+            isset($title) && !empty($title)
+            &&
+            isset($body) && !empty($body)
+        ) {
 
-                    require_once 'app/core/database/models.php';
-                    require_once 'app/utils/methods.php';
-                    
-                     $database = new Model();
-                     
-                     $table = 'post';
-                     $fields = 'title, slug, body, banner, author';
-                     $values = '?,?,?,?,?';
-                    $slug = generate_slug($title);
-                    $banner = file_upload('app/media/', 'banner');
-                    $author = $_SESSION['user_info'][0]['id'];
+            require_once 'app/core/database/models.php';
+            require_once 'app/utils/methods.php';
 
-                    
+            $database = new Model();
 
-                    $data = array($title, $slug, $body, $banner, $author );
-                    $database -> add($table, $fields, $values, $data);
-                    echo '<script>alert("Succes")</script>';
+            $table = 'post';
+            $fields = 'title, slug, body, banner, author';
+            $values = '?,?,?,?,?';
+            $slug = generate_slug($title);
+            $banner = file_upload('app/media/', 'banner');
+            $author = $_SESSION['user_info'][0]['id'];
 
-                }
-                else {
-                    echo '<script>alert("Tous les champs sont requis")</script>';
-                }
 
+
+            $data = array($title, $slug, $body, $banner, $author);
+            $database->add($table, $fields, $values, $data);
+            echo '<script>alert("Succes")</script>';
+        } else {
+            echo '<script>alert("Tous les champs sont requis")</script>';
         }
+    }
 
 
-        public function get_all(){
+    public function get_all()
+    {
+
+        require_once 'app/core/database/models.php';
+        $database = new Model();
+
+        $table = 'post';
+        $fields = '*';
+
+        $posts = $database->read($table, $fields);
+
+        while ($post = $posts->fetch()) {
+
+
+            echo '<a href="post?slug=' . $post['slug'] . '"> ' . $post['title'] . '</a> <br>';
+        }
+    }
+    public function details($slug)
+    {
+
+        require_once 'app/core/database/models.php';
+        $database = new Model();
+        $table = 'post';
+        $fields = '*';
+        $sfields = 'slug';
+        $values = array($slug);
+        $query = $database->read_filter_once($table, $fields, $sfields, $values);
+
+        $post = $query->fetch();
+        return $post;
+    }
+
+    public function add_comment($body, $author, $post){
+
+            if( isset($body) && !empty($body)){
 
                 require_once 'app/core/database/models.php';
+
                 $database = new Model();
 
-                $table = 'post';
-                $fields = '*';
+                $table = 'comment';
+                $fields = 'body, author, post';
+                $values = '?,?,?';
+              
+                $data = array($body, $author, $post);
 
-                $posts = $database->read($table, $fields);
+                $database->add($table, $fields, $values, $data);
 
-               while($post=$posts->fetch()){
+                echo '<script>alert("Votre commentaire a été ajouté avec succes")</script>';
 
-                
-                echo '<a href="post?slug='.$post['slug'].'"> '.$post['title'].'</a> <br>';
+            }
 
-               }
-        }
-    
     }
-?>
-
+}
